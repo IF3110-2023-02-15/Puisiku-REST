23be0963f5ae72d29e35bcf889f898a296684adc
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-29 00:38:39 +0700

    feat: subscription service

---
+++ src/services/subscription.service.ts
@@ -0,0 +1,14 @@
+/**
+ * Subscription Service with SOAP
+ */
+class SubscriptionService {
+  constructor() {}
+
+  async subscribe(email: string) {}
+
+  async checkSubscription(email: string) {
+    return { subscribed: true }
+  }
+}
+
+export default SubscriptionService
