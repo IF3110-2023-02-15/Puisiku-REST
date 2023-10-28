947df94bc311a9f17a02bb4ec81907995d96060f
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-28 23:29:47 +0700

    feat: Login and Register Dto

---
+++ src/domains/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const LoginDto = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+export const RegisterDto = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
