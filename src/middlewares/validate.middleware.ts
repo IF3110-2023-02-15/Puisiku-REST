022b06e1e806345bb2ff79c3a64110be2ec7b279
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-10-28 23:31:29 +0700

    feat: request body validation middleware

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express'
+import { ZodSchema, ZodError } from 'zod'
+
+export const validate = (schema: ZodSchema<any>) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body)
+      next()
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res
+          .status(400)
+          .send({
+            message:
+              'Request body validation failed! Please send appropriate request body!',
+          })
+      } else {
+        res.status(500).send({
+          message:
+            'An unexpected error occurred in request validation! Please try again later!',
+        })
+      }
+    }
+  }
+}
