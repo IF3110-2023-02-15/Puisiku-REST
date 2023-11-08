08a595473be345e454e9c26fe5fda0b9ebbe34cc
auliamey <auliaannandya@gmail.com>
2023-11-08 15:32:42 +0700

    add poem domain

---
+++ src/domains/poem.domain.ts
@@ -0,0 +1,11 @@
+export interface IPoem {
+    title: string
+    creatorId: number
+    genre: string
+    content: string
+    imagePath: string
+    audioPath: string
+    year: number
+    createdAt: Date
+    updatedAt: Date
+}
