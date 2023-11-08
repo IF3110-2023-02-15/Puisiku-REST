9897b5d93ec49185884624e4899afd1362e41a0a
auliamey <auliaannandya@gmail.com>
2023-11-08 15:32:00 +0700

    seed data

---
+++ prisma/user.ts
@@ -0,0 +1,52 @@
+export const users = [
+    {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      hashedPassword: 'hashed_password_1',
+    },
+    {
+      name: 'Jane Smith',
+      email: 'janesmith@example.com',
+      hashedPassword: 'hashed_password_2',
+    },
+    {
+      name: 'Michael Johnson',
+      email: 'michaeljohnson@example.com',
+      hashedPassword: 'hashed_password_3',
+    },
+    {
+      name: 'Emily Davis',
+      email: 'emilydavis@example.com',
+      hashedPassword: 'hashed_password_4',
+    },
+    {
+      name: 'William Wilson',
+      email: 'williamwilson@example.com',
+      hashedPassword: 'hashed_password_5',
+    },
+    {
+      name: 'Olivia Jones',
+      email: 'oliviajones@example.com',
+      hashedPassword: 'hashed_password_6',
+    },
+    {
+      name: 'Daniel Anderson',
+      email: 'danielanderson@example.com',
+      hashedPassword: 'hashed_password_7',
+    },
+    {
+      name: 'Sophia Martinez',
+      email: 'sophiamartinez@example.com',
+      hashedPassword: 'hashed_password_8',
+    },
+    {
+      name: 'Alexis Taylor',
+      email: 'alexistaylor@example.com',
+      hashedPassword: 'hashed_password_9',
+    },
+    {
+      name: 'James Thomas',
+      email: 'jamesthomas@example.com',
+      hashedPassword: 'hashed_password_10',
+    },
+  ];
