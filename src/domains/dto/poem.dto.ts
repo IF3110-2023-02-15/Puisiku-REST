6319c0ebbfef46ce85c15b72346e09f52649a1bb
auliamey <auliaannandya@gmail.com>
2023-11-10 13:06:38 +0700

    poem sql and fix dto

--- src/domains/dto/poem.dto.ts
+++ src/domains/dto/poem.dto.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod'
 
-export const PoemDto = z.object({
+export const addPoemDto = z.object({
   title: z.string(),
   genre: z.string(),
   content: z.string(),
+  creatorId: z.number(),
   imagePath: z.string(),
   audioPath: z.string(),
-  year: z.string(),
+  year: z.number(),
 })
-
