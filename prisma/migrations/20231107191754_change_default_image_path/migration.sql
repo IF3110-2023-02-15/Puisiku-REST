c874069250ab829c0dd56eb7f7abeea20ffd823a
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-10 16:38:55 +0700

    fix: resolve conflict in migrations

--- prisma/migrations/20231107191754_change_default_image_path/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "imagePath" SET DEFAULT '/img/default_user.png';
--- prisma/migrations/20231110092855_resolve_unconsistency/migration.sql
+++ prisma/migrations/20231110092855_resolve_unconsistency/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "hashedPassword" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "imagePath" VARCHAR(255) NOT NULL DEFAULT '/img/default_user.png',
+    "description" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Poem" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
@@ -13,6 +26,9 @@ CREATE TABLE "Poem" (
 
     CONSTRAINT "Poem_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Poem" ADD CONSTRAINT "Poem_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
