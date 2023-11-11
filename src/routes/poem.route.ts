f34b5a30cc5fcc67df49c800180aeea97834b78c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-11 18:21:59 +0700

    chore: add getAlbumPoems route

--- src/routes/poem.route.ts
+++ src/routes/poem.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import { authenticate } from '../middlewares/auth.middleware'
 import PoemController from '../controllers/poem.controller'
 import { validate } from '../middlewares/validate.middleware'
 import { addPoemDto } from '../domains/dto/poem.dto'
@@ -8,8 +9,9 @@ const router = Router()
 
 const poemController = new PoemController()
 
-router.get('/', poemController.getAllPoems)
-router.get('/:id', poemController.getPoemById)
-router.post('/', validate(addPoemDto), poemController.createPoem)
+router.get('/album/:albumId', authenticate, poemController.getAlbumPoems)
+router.get('/', authenticate, poemController.getAllPoems)
+router.get('/:id', authenticate, poemController.getPoemById)
+router.post('/', authenticate, validate(addPoemDto), poemController.createPoem)
 
 export default router
