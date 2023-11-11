20fb9cc57a25dc4d4da47cb4bc0de045fa74569b
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-11 18:22:40 +0700

    chore: add getAlbumPoems dto fix: dto body

--- src/domains/dto/poem.dto.ts
+++ src/domains/dto/poem.dto.ts
@@ -1,11 +1,22 @@
 import { z } from 'zod'
 
 export const addPoemDto = z.object({
-  title: z.string(),
-  genre: z.string(),
-  content: z.string(),
-  creatorId: z.number(),
-  imagePath: z.string(),
-  audioPath: z.string(),
-  year: z.number(),
+  body: z.object({
+    title: z.string(),
+    genre: z.string(),
+    content: z.string(),
+    creatorId: z.number(),
+    imagePath: z.string(),
+    audioPath: z.string(),
+    year: z.number(),
+    albumId: z.number(),
+  }),
+})
+
+export const getAlbumPoems = z.object({
+  params: z.object({
+    albumId: z.string().refine((albumId) => !isNaN(Number(albumId)), {
+      message: 'Album id is not a number',
+    }),
+  }),
 })
