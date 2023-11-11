72965e77921c8699432f6696d145cfbaa4447016
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-11 18:21:49 +0700

    chore: add getAlbumPoems

--- src/services/poem.service.ts
+++ src/services/poem.service.ts
@@ -1,20 +1,24 @@
-import PoemRepository from "../repositories/poem.repository";
+import PoemRepository from '../repositories/poem.repository'
 import { IPoem } from '../domains/poem.domain'
 
 class PoemService {
-    constructor(private poemRepository: PoemRepository) {}
+  constructor(private poemRepository: PoemRepository) {}
 
-    async getAllPoems() {
-        return await this.poemRepository.getAllPoems()
-    }
+  async getAllPoems() {
+    return await this.poemRepository.getAllPoems()
+  }
 
-    async createPoem(data: IPoem) {
-        return await this.poemRepository.createPoem(data)
-    }
+  async createPoem(data: IPoem) {
+    return await this.poemRepository.createPoem(data)
+  }
 
-    async getPoemById(id: number){
-        return await this.poemRepository.getPoemById(id)
-    }
+  async getPoemById(id: number) {
+    return await this.poemRepository.getPoemById(id)
+  }
+
+  async getAlbumPoems(albumId: number) {
+    return await this.poemRepository.getPoemsByAlbum(albumId)
+  }
 }
 
 export default PoemService
