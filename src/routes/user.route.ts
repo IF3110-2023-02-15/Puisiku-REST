79b5b72eb7923b336543a288c08ed426175601a2
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-11-14 20:27:46 +0700

    chore: get Creator By Id

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,13 +3,19 @@ import { Router } from 'express'
 import { validate } from '../middlewares/validate.middleware'
 import { authenticate } from '../middlewares/auth.middleware'
 import UserController from '../controllers/user.controller'
-import { updateProfileDto } from '../domains/dto/user.dto'
+import { updateProfileDto, getCreatorByIdDto } from '../domains/dto/user.dto'
 
 const router = Router()
 
 const userController = new UserController()
 
 router.get('/creators', authenticate, userController.getCreators)
+router.get(
+  '/creator/:creatorId',
+  authenticate,
+  validate(getCreatorByIdDto),
+  userController.getCreatorById
+)
 router.get('/', authenticate, userController.getProfile)
 router.patch(
   '/',
