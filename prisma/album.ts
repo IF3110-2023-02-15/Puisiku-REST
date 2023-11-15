410dfd154ed7476a8bc8da7b327197e5ba7130be
auliamey <auliaannandya@gmail.com>
2023-11-15 17:39:50 +0700

    add seeding

--- prisma/album.ts
+++ prisma/album.ts
@@ -1,52 +1,57 @@
 export const albums = [
     {
         name: 'Get To Know Me',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
+        creatorId: 1,
+    },
+    {
+        name: 'Labyrinth',
+        imagePath: '/img/default_album.png',
         creatorId: 1,
     },
     {
         name: 'I Love You',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 2,
     },
     {
         name: 'Clean Mind',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 3,
     },
     {
         name: 'New Romantics',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 4,
     },
     {
         name: 'First Album',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 5,
     },
     {
         name: 'None of Me',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 6,
     },
     {
         name: 'Build For You',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 7,
     },
     {
         name: 'Like A Dream',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 8,
     },
     {
         name: 'National Anthem',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 9,
     },
     {
         name: 'Best People',
-        imagePath: '/img/1699529759176_3.png',
+        imagePath: '/img/default_album.png',
         creatorId: 10,
     },
 ]
